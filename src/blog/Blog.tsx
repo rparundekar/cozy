50696719a63d36b788ba0472c281202ecc33f55a
valbuena <valbuena@duck.com>
2025-03-22 00:24:36 +0100

    number x string

--- src/blog/Blog.tsx
+++ src/blog/Blog.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 interface Post {
-  id: number;
+  id: string;
   title: string;
   date: string;
   tags: string[];
